20be91dc943f6e04ddf9358589064f9f0388faad
dav-and-co <claude.daviatte@lapiscine.pro>
2024-08-15 13:38:58 +0200

    20240813 chevron microcredit

--- public/JS/microcredit.js
+++ public/JS/microcredit.js
@@ -4,10 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
     toggles.forEach(function (toggle) {
         toggle.addEventListener('click', function () {
             const content = this.nextElementSibling;
+            const chevrons = this.querySelectorAll('.chevron');
+
+            // Alterne l'affichage du contenu
             if (content.style.display === 'block') {
                 content.style.display = 'none';
+                chevrons[0].style.display = 'flex'; // Affiche le chevron bas
+                chevrons[1].style.display = 'none';  // Masque le chevron haut
             } else {
                 content.style.display = 'block';
+                chevrons[0].style.display = 'none';  // Masque le chevron bas
+                chevrons[1].style.display = 'flex'; // Affiche le chevron haut
             }
         });
     });
