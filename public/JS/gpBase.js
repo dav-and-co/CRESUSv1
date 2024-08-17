4926f550a26b704bd18fad0bb77382e77de0ab76
dav-and-co <claude.daviatte@lapiscine.pro>
2024-08-17 16:34:00 +0200

    20240813 debur scss

--- public/JS/gpBase.js
+++ public/JS/gpBase.js
@@ -1,7 +1,7 @@
 let nav = document.querySelector('nav')
 window.addEventListener('scroll', () => {
     if(window.scrollY > 20){
-        nav.style.backgroundColor = "#F7FAFB";
+        nav.style.backgroundColor = "#f0f3f9";
     }
     else{
         nav.style.backgroundColor = "transparent";
